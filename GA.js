a4f521bb454eda95df72f5f8ba31bcf3283b3124
unknown <cardielbutler@gmail.com>
2019-03-20 09:28:06 -0400

    second commit

---
+++ GA.js
@@ -0,0 +1,16 @@
+// base of api//
+const baseUrl = "https://ghibliapi.herokuapp.com"
+let url;
+const species = "/species?"
+fetch(baseUrl)
+
+function test() {
+    console.log(('Hello WOrld'))
+};
+
+
+
+// button actions //
+button.addEventListener('click',test()); 
+
+    button.onclick = test();
